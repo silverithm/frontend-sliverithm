041f7290fbd68f9cb48dfba33cc16dcdef0e1c53
bicco2 <jhb_0921@naver.com>
2024-04-05 02:56:57 +0900

    chore : Initial project setup (+ normalize.css)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // TODO: test 용도
+
+function App() {
+  return (
+    <div className="main-page">
+      <h1>환영합니다!</h1>
+      <p>이것은 간단한 메인 페이지입니다. 테스트 용도로 만들어졌습니다.</p>
+    </div>
+  );
+}
+
+export default App;
